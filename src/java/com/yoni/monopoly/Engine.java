a13da60809c8e040a6aa9d7cbaf561c3096d4743
Yoni <yonilev100@gmail.com>
2018-02-13 18:23:25 +0200

    Added players

--- src/java/com/yoni/monopoly/Engine.java
+++ src/java/com/yoni/monopoly/Engine.java
@@ -3,20 +3,35 @@ package com.yoni.monopoly;
 import com.yoni.monopoly.dice.DiceManager;
 import com.yoni.monopoly.dice.DiceResult;
 
+import java.util.ArrayList;
+
 public class Engine {
 
     private final DiceManager diceManager;
+    private final ArrayList<Player> playerList;
+
+    private int currentPlayer;
 
 
     public Engine() {
         diceManager = new DiceManager();
+
+        playerList = new ArrayList<>();
+        playerList.add(new Player("red"));
+        playerList.add(new Player("green"));
+        playerList.add(new Player("blue"));
+        playerList.add(new Player("yellow"));
     }
 
 
     public void run() {
         System.out.println("Running single turn");
 
-        DiceResult diceResult = diceManager.throwDice();
-        System.out.println("Throwing dice, result=" + diceResult.getDice1() + ", " + diceResult.getDice2());
+        for (Player player : playerList) {
+            DiceResult diceResult = diceManager.throwDice();
+            System.out.println("Throwing dice, result = " + diceResult.getDice1() + ", " + diceResult.getDice2());
+            System.out.println("the "+player.getColor()+" player moved " + diceResult.getDice1() + ", " + diceResult.getDice2());
+
+        }
     }
 }
---
+++ src/java/com/yoni/monopoly/Player.java
@@ -0,0 +1,12 @@
+package com.yoni.monopoly;
+
+public class Player {
+    String color;
+    public Player(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
