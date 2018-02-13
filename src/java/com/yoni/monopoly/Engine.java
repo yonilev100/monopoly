6015f36915410d5b3417056d150e3b3d0deb59f1
Yoni <yonilev100@gmail.com>
2018-02-13 18:53:13 +0200

    Added locations

--- src/java/com/yoni/monopoly/Engine.java
+++ src/java/com/yoni/monopoly/Engine.java
@@ -9,9 +9,8 @@ public class Engine {
 
     private final DiceManager diceManager;
     private final ArrayList<Player> playerList;
-
-    private int currentPlayer;
-
+    String locations[] = {"Ben-gurion", "Bialik", "Negba", "Hevrat te'ufa", "Otobus", "Hanasi", "Alanbi", "Dereh-hayam"};
+    int playerIndex = 0;
 
     public Engine() {
         diceManager = new DiceManager();
@@ -26,12 +25,17 @@ public class Engine {
 
     public void run() {
         System.out.println("Running single turn");
+        Player player = playerList.get(playerIndex);
 
-        for (Player player : playerList) {
-            DiceResult diceResult = diceManager.throwDice();
-            System.out.println("Throwing dice, result = " + diceResult.getDice1() + ", " + diceResult.getDice2());
-            System.out.println("the "+player.getColor()+" player moved " + diceResult.getDice1() + ", " + diceResult.getDice2());
-
+        DiceResult diceResult = diceManager.throwDice();
+        System.out.println("Throwing dice, result = " + diceResult.getDice1() + ", " + diceResult.getDice2());
+        System.out.println("the " + player.getColor() + " player moved " + diceResult.getDice1() + ", " + diceResult.getDice2());
+        player.setIndex((player.getIndex() + diceResult.getDice1() + diceResult.getDice2()) % 8);
+        System.out.println("the player is on " + locations[player.getIndex()]);
+        playerIndex++;
+        if (playerIndex >= 4) {
+            playerIndex = 0;
+            playerIndex++;
         }
     }
 }
--- src/java/com/yoni/monopoly/Player.java
+++ src/java/com/yoni/monopoly/Player.java
@@ -2,11 +2,20 @@ package com.yoni.monopoly;
 
 public class Player {
     String color;
+    int index = 0;
     public Player(String color) {
         this.color = color;
     }
 
     public String getColor() {
         return color;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public int getIndex() {
+        return index;
     }
 }
